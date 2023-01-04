43262dc2615064c85d45b5ffef6f3e7514dfaefe
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-01-04 22:52:54 +0100

    added new vscode extensions

--- vscode/install_vscode_extensions.ps1
+++ vscode/install_vscode_extensions.ps1
@@ -13,6 +13,11 @@ code --install-extension ms-toolsai.jupyter-renderers
 code --install-extension ms-toolsai.vscode-jupyter-cell-tags
 code --install-extension ms-toolsai.vscode-jupyter-slideshow
 code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode.remote-explorer
 code --install-extension njpwerner.autodocstring
 code --install-extension PKief.material-icon-theme
 code --install-extension yzhang.markdown-all-in-one
