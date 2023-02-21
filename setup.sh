1ceadacce6e56d8cd6ec1324508bfa61bbe67808
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-02-22 00:52:33 +0100

    aggiunta installazione nvm e node lts

--- setup.sh
+++ setup.sh
@@ -4,4 +4,11 @@ _pwd=$(pwd)
 ln -sf ${_pwd}/linux_terminal/.vimrc $HOME/.vimrc
 ln -sf ${_pwd}/linux_terminal/.git.plugin.sh $HOME/.git.plugin.sh
 ln -sf ${_pwd}/linux_terminal/.luzzi_theme.omp.json $HOME/.luzzi_theme.omp.json
+if ! nvm;then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+fi
 source ~/.bashrc
+
+if ! node;then
+    nvm install --lts
+;fi
