d054d02eeeebec520bd98c91ad40a395b317977a
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-03-22 00:13:00 +0100

    added tmux configuration, TODO better setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,15 @@ _pwd=$(pwd)
 ln -sf ${_pwd}/linux_terminal/.vimrc $HOME/.vimrc
 ln -sf ${_pwd}/linux_terminal/.git.plugin.sh $HOME/.git.plugin.sh
 ln -sf ${_pwd}/linux_terminal/.luzzi_theme.omp.json $HOME/.luzzi_theme.omp.json
+ln -sf ${_pwd}/linux_terminal/.tmux.conf $HOME/.tmux.conf
 if ! nvm;then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 fi
 source ~/.bashrc
-
 if ! node;then
     nvm install --lts
-;fi
+fi
+
+if [ ! -d ~/.tmux/plugins/tpm ];then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
