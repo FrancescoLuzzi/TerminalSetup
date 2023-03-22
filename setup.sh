cd87d9110c8497eb098637bf0219066415a2dcca
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-03-22 01:03:35 +0100

    tmux install plugins

--- setup.sh
+++ setup.sh
@@ -15,4 +15,14 @@ fi
 
 if [ ! -d ~/.tmux/plugins/tpm ];then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    # start a server but don't attach to it
+    tmux start-server
+    # source .tmux.conf
+    tmux source $HOME/.tmux.source
+    # create a new session but don't attach to it either
+    tmux new-session -d
+    # install the plugins
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+    # killing the server is not required, I guess
+    tmux kill-server
 fi
