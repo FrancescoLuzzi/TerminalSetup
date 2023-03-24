a3eaa71849648d89508868ff31d31d4fba0f45e4
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-03-24 19:08:37 +0100

    fix setup ping, nvm, tmux and node install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# load nvm just to be sure
+source ~/.nvm/nvm.sh 2>/dev/null
+
 _pwd=$(pwd)
 ln -sf ${_pwd}/linux_terminal/.vimrc $HOME/.vimrc
 ln -sf ${_pwd}/linux_terminal/.git.plugin.sh $HOME/.git.plugin.sh
@@ -8,17 +11,17 @@ ln -sf ${_pwd}/linux_terminal/.tmux.conf $HOME/.tmux.conf
 
 is_ping_usable=$(ping -q -c 1 -W 1 8.8.8.8 1>/dev/null 2>&1; echo $?)
 
-if [ $is_ping_usable = "130" ];then
+if [ $is_ping_usable = "2" ];then
     echo "Enabling use of ping in wsl"
     sudo setcap cap_net_raw+p /bin/ping
 fi
 
-if ! nvm;then
+if ! nvm --version;then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
     source ~/.nvm/nvm.sh
 fi
 
-if ! node;then
+if ! node --version;then
     nvm install --lts
 fi
 
@@ -27,7 +30,7 @@ if [ ! -d ~/.tmux/plugins/tpm ];then
     # start a server but don't attach to it
     tmux start-server
     # source .tmux.conf
-    tmux source $HOME/.tmux.source
+    tmux source $HOME/.tmux.conf
     # create a new session but don't attach to it either
     tmux new-session -d
     # install the plugins
