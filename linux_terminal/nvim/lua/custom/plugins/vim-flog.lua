0c551e74540aa41a6dfed64fc89cbd9ca6080bca
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-07-13 14:55:24 +0200

    nvim: added vim-illuminate and fixed vim-flog

--- linux_terminal/nvim/lua/custom/plugins/vim-flog.lua
+++ linux_terminal/nvim/lua/custom/plugins/vim-flog.lua
@@ -2,7 +2,7 @@ return {
 	'rbong/vim-flog',
 	init = function()
 		vim.keymap.set(
-			'n', '<leader>gg', '<cmd>Flog',
+			'n', '<leader>gg', '<cmd>Flog<CR>',
 			{ desc = '[G]it [G]raph' })
 	end
 }
---
+++ linux_terminal/nvim/lua/custom/plugins/vim-illuminate.lua
@@ -0,0 +1,25 @@
+return {
+	'RRethy/vim-illuminate',
+	init = function()
+		require('illuminate').configure({
+			providers = {
+				'lsp',
+				'treesitter',
+				'regex',
+			},
+		})
+		vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#4d5474" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#4d5474" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#4d5474" })
+
+		--- auto update the highlight style on colorscheme change
+		vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+			pattern = { "*" },
+			callback = function(ev)
+				vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#4d5474" })
+				vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#4d5474" })
+				vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#4d5474" })
+			end
+		})
+	end
+}
