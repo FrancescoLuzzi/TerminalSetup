fefbb4fb90b8d37e7bd7db0fbb2d954a5963ad79
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-07-25 18:12:52 +0200

    update lazy.lock and added preview or files in telescope

--- linux_terminal/nvim/init.lua
+++ linux_terminal/nvim/init.lua
@@ -262,7 +262,7 @@ vim.keymap.set('n', '<leader>sb', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
-    previewer = false,
+    previewer = true,
   })
 end, { desc = 'Fuzzily [s]earch in current [b]uffer' })
 
