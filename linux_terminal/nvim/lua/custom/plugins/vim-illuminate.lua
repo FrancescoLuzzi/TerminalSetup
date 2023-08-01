30258b3e504e2e0442db743a061d1a2ea35d8602
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-08-01 11:09:14 +0200

    disable vim-illuminate when using NerdTree

--- linux_terminal/nvim/lua/custom/plugins/vim-illuminate.lua
+++ linux_terminal/nvim/lua/custom/plugins/vim-illuminate.lua
@@ -3,6 +3,10 @@ return {
 	init = function()
 		require('illuminate').configure({
 			delay = 250,
+			filetypes_denylist = {
+				'fugitive',
+				'NvimTree'
+			},
 		})
 		vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#4d5474" })
 		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#4d5474" })
