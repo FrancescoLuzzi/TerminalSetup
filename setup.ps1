731e62809d33667c400fb11191c4cce0b9f5e9bd
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-08-16 15:49:59 +0200

    added -vscode_settings to setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,15 @@
 param(
   [switch]$vscode,
+  [switch]$vscode_settings,
   [switch]$theme,
   [switch]$wsl,
   [switch]$neovim
 )
 
 # install vscode
-if ($vscode){
+if ($vscode) {
   code --version >$null
-  if (! $?){
+  if (! $?) {
     $build = 'stable'
     $os = 'win32-x64-user'
     $SourceURL = "https://code.visualstudio.com/sha/download?build=$build&os=$os";
@@ -19,25 +20,33 @@ if ($vscode){
     Remove-Item $Installer;
   }
   ./vscode/install_vscode_extensions.ps1
-  New-Item -ItemType HardLink -Path $env:APPDATA/Code/User/keybindings.json -Target $HOME/.terminal_setup/vscode/keybindings.json -Force
-  New-Item -ItemType HardLink -Path $env:APPDATA/Code/User/settings.json -Target $HOME/.terminal_setup/vscode/settings.json -Force
 }
+
+# set vscode bindings when either one of the vscode switch are set
+if ($vscode_settings -or $vscode) {
+  code --version >$null
+  if ($?) {
+    New-Item -ItemType HardLink -Path $env:APPDATA/Code/User/keybindings.json -Target $HOME/.terminal_setup/vscode/keybindings.json -Force
+    New-Item -ItemType HardLink -Path $env:APPDATA/Code/User/settings.json -Target $HOME/.terminal_setup/vscode/settings.json -Force
+  }
+}
+
 # set theme
-if ($theme){
-Copy-Item -Force $HOME/.terminal_setup/theme/DarkWolf.theme $HOME/AppData/Local/Microsoft/Windows/Themes/DarkWolf.theme
-Copy-Item -Recurse -Force $HOME/.terminal_setup/theme/DesktopBackground $HOME/AppData/Local/Microsoft/Windows/Themes/RoamedThemeFiles
+if ($theme) {
+  Copy-Item -Force $HOME/.terminal_setup/theme/DarkWolf.theme $HOME/AppData/Local/Microsoft/Windows/Themes/DarkWolf.theme
+  Copy-Item -Recurse -Force $HOME/.terminal_setup/theme/DesktopBackground $HOME/AppData/Local/Microsoft/Windows/Themes/RoamedThemeFiles
 }
 
 # setup wsl
-if ($wsl){
+if ($wsl) {
   wsl --install -d Debian
-  echo "After setting up for the first time your wsl instance you can follow the linux tutorial to set up that enviroment!"
+  Write-Host "After setting up for the first time your wsl instance you can follow the linux tutorial to set up that enviroment!"
 }
 
 # setup neovim
-if ($neovim){
+if ($neovim) {
   gcc --version >$null
-  if (! $?){
+  if (! $?) {
     Write-Warning "Can't continue with Neovim installation.`nMingw[32|64] not installed, download from https://winlibs.com/#download-release and add it's bin folder to `$PATH"
     return
   }
