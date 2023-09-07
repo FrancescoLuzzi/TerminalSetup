9d6d4ba4fe5b41be82046af58d6efa0c1f84d83b
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-09-07 15:31:54 +0200

    nvim: splitted find_files and git_files

--- linux_terminal/nvim/init.lua
+++ linux_terminal/nvim/init.lua
@@ -263,7 +263,7 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sB', require('telescope.builtin').buffers, { desc = '[S]earch existing [B]uffers' })
 vim.keymap.set('n', '<leader>sb', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -274,16 +274,9 @@ end, { desc = 'Fuzzily [s]earch in current [b]uffer' })
 
 -- smart telescope file search
 
-local function find_project_files(opts)
-  opts = opts or {}
-  local ok = pcall(require('telescope.builtin').git_files, opts)
 
-  if not ok then
-    require('telescope.builtin').find_files(opts)
-  end
-end
-
-vim.keymap.set('n', '<leader>sf', find_project_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sF', require('telescope.builtin').git_files, { desc = '[S]earch Git [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
