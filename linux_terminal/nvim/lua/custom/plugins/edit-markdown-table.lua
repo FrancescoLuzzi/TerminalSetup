bbfbb9e545edbc235ac91a38b010a760f7900f46
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-10-12 18:02:32 +0200

    nvim: added edit-markdown-table, to edit table cells easily

---
+++ linux_terminal/nvim/lua/custom/plugins/edit-markdown-table.lua
@@ -0,0 +1,9 @@
+return {
+  'kiran94/edit-markdown-table.nvim',
+  config = true,
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  cmd = "EditMarkdownTable",
+  init = function()
+    vim.keymap.set('n', '<leader>te', ':EditMarkdownTable<CR>', { desc = '[T]oggle Markdown [E]dit', silent = true, })
+  end
+}
