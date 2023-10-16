556ae924028df0ab002dceaaa6f3ab8af05c7c17
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-10-16 22:16:54 +0200

    fix go.nvim config after update

--- linux_terminal/nvim/lua/custom/plugins/go.lua
+++ linux_terminal/nvim/lua/custom/plugins/go.lua
@@ -14,10 +14,11 @@ return {
       goimport = "gopls",   -- goimport command, can be gopls[default] or goimport
       fillstruct = "gopls", -- can be nil (use fillstruct, slower) and gopls
       gofmt = "gofumpt",    -- gofmt cmd,
-      lsp_diag_underline = false,
-      lsp_codelens = true,  -- set to false to disable codelens, true by default
-      lsp_keymaps = false,  -- set to false to disable gopls/lsp keymap
-      lsp_diag_hdlr = true, -- hook lsp diag handler
+      diagnostic = {
+        underline = false,
+      },
+      lsp_codelens = true, -- set to false to disable codelens, true by default
+      lsp_keymaps = false, -- set to false to disable gopls/lsp keymap
       lsp_document_formatting = false,
       lsp_inlay_hints = {
         enable = false
