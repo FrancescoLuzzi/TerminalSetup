dd5e24597763effb877ef5a5afbf6c4e01e67c0e
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-10-27 16:55:06 +0200

    telescope branches

--- linux_terminal/nvim/init.lua
+++ linux_terminal/nvim/init.lua
@@ -323,6 +323,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').lsp_references, { desc = '[S]earch [R]eferences' })
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').git_branches, { desc = '[S]earch [B]ranches' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
