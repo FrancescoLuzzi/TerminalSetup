b715c1a4db11bca1990cebd77423804f2ca82b3b
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-11-07 12:49:00 +0100

    autopairs removed automatically adding parents after selecting function/method

--- linux_terminal/nvim/lua/custom/plugins/autopairs.lua
+++ linux_terminal/nvim/lua/custom/plugins/autopairs.lua
@@ -7,11 +7,11 @@ return {
 			disable_filetype = { "sh", "bash", "TelescopePrompt" },
 		})
 		-- If you want to automatically add `(` after selecting a function or method
-		local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-		local cmp = require('cmp')
-		cmp.event:on(
-			'confirm_done',
-			cmp_autopairs.on_confirm_done()
-		)
+		-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+		-- local cmp = require('cmp')
+		-- cmp.event:on(
+		-- 	'confirm_done',
+		-- 	cmp_autopairs.on_confirm_done()
+		-- )
 	end,
 }
