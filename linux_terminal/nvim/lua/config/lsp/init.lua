ba35f69fa991306c5a3980aa3e1603470a08a400
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-11-19 00:17:19 +0100

    neovim: added emmet_ls

--- linux_terminal/nvim/lua/config/lsp/init.lua
+++ linux_terminal/nvim/lua/config/lsp/init.lua
@@ -164,6 +164,18 @@ local servers = {
   docker_compose_language_service = {},
   dockerls = {},
   html = {},
+  -- https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+  emmet_ls = {
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    },
+  },
   jsonls = {},
   marksman = {},
   sqlls = {},
