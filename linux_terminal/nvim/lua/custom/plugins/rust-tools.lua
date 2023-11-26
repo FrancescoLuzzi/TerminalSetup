dfbabb85d62187b4262880cf04c57f5a4f2a5ea9
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-11-26 01:29:06 +0100

    neovim update crates.nvim

--- linux_terminal/nvim/lua/custom/plugins/rust-tools.lua
+++ linux_terminal/nvim/lua/custom/plugins/rust-tools.lua
@@ -5,16 +5,13 @@ return {
 		'nvim-lua/popup.nvim',
 		{
 			"saecki/crates.nvim",
-			version = "v0.3.0",
+			version = "v0.4.0",
 			dependencies = { "nvim-lua/plenary.nvim" },
 			init = function()
 				require("crates").setup {
-					-- null_ls = {
-					-- 	enabled = true,
-					-- 	name = "crates.nvim",
-					-- },
 					popup = {
 						border = "rounded",
+						autofocus = true,
 					},
 				}
 			end
