9e76cabad9bb6d89b9aea97ae3c8ed26d2db33c5
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2023-11-27 10:37:17 +0100

    neovim added python keymaps

--- linux_terminal/nvim/lua/config/lsp/python.lua
+++ linux_terminal/nvim/lua/config/lsp/python.lua
@@ -4,11 +4,45 @@ local M = {}
 local group_name = 'kickstart-lsp-format-pyright'
 local group = vim.api.nvim_create_augroup(group_name, { clear = true })
 
+local function pip_install()
+  vim.ui.input(
+    { prompt = 'Install packages (separated by space): ' },
+    function(packages)
+      vim.cmd("!pip install " .. packages)
+    end
+  )
+end
+
+local function pip_freeze()
+  if vim.loop.os_uname().sysname:match("Windows") then
+    vim.cmd('!pip freeze | Out-File -Encoding UTF8 ' .. vim.loop.cwd() .. '/requirements.txt')
+  else
+    vim.cmd('!pip freeze > ' .. vim.loop.cwd() .. '/requirements.txt')
+  end
+end
+
 local function configure_keymappings(bufnr)
+  -- attach my LSP configs keybindings
+  local wk = require("which-key")
+  local default_options = { silent = true }
+  wk.register({
+    c = {
+      name = "PythonCoding",
+      p = {
+        name = "Pip commands",
+        i = { pip_install, "Install package" },
+        r = { pip_freeze, "Generate requirements.txt" },
+
+      },
+    },
+  }, { prefix = "<leader>", mode = "n", default_options })
+
   vim.api.nvim_create_autocmd("BufWritePre", {
     group = group,
     buffer = bufnr,
     callback = function(args)
+      -- configuration in conform plugin setup
+      -- black + isort
       require("conform").format({ bufnr = args.buf })
     end,
   })
