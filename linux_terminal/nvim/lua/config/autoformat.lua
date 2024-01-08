77a7b5bfb4781873c3bda7a309c53f57fc3f940d
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2024-01-08 16:18:29 +0100

    <leader>th to ToggleFormat

--- linux_terminal/nvim/lua/config/autoformat.lua
+++ linux_terminal/nvim/lua/config/autoformat.lua
@@ -8,7 +8,7 @@ return {
       format_is_enabled = not format_is_enabled
       print('Setting autoformatting to: ' .. tostring(format_is_enabled))
     end, {})
-
+    vim.keymap.set('n', '<leader>tf', ':KickstartFormatToggle<CR>', { desc = 'Toggle Formatting', silent = true })
     -- Create an augroup that is used for managing our formatting autocmds.
     --      We need one augroup per client to make sure that multiple clients
     --      can attach to the same buffer without interfering with each other.
