33a54fa76e48e16d8a22dee59ad6533cbf952047
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2024-02-12 12:17:31 +0100

    nvim: jj to exit insert mode

--- linux_terminal/nvim/init.lua
+++ linux_terminal/nvim/init.lua
@@ -288,6 +288,9 @@ require('lazy').setup({
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- exit insert mode with 'jj'
+vim.keymap.set('i', 'jj', '<Esc>', { silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
