dc4c161bd18c1e9d2dde61befb4a7c50eefde2c9
FrancescoLuzzi <francescoluzzi99@libero.it>
2024-04-07 13:12:55 +0200

    fix illuminate bg

--- linux_terminal/nvim/lua/custom/plugins/vim-illuminate.lua
+++ linux_terminal/nvim/lua/custom/plugins/vim-illuminate.lua
@@ -14,17 +14,18 @@ return {
         'toggleterm',
       },
     })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordText', { bg = '#4d5474' })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { bg = '#4d5474' })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { bg = '#4d5474' })
+    local illuminate_bg = '#4f4536'
+    vim.api.nvim_set_hl(0, 'IlluminateWordText', { bg = illuminate_bg })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { bg = illuminate_bg })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { bg = illuminate_bg })
 
     --- auto update the highlight style on colorscheme change
     vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
       pattern = { '*' },
       callback = function(ev)
-        vim.api.nvim_set_hl(0, 'IlluminatedWordText', { bg = '#4d5474' })
-        vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { bg = '#4d5474' })
-        vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { bg = '#4d5474' })
+        vim.api.nvim_set_hl(0, 'IlluminatedWordText', { bg = illuminate_bg })
+        vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { bg = illuminate_bg })
+        vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { bg = illuminate_bg })
       end,
     })
   end,
