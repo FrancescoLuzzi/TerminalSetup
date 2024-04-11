ea3270fe1ec3ee377f81060ced5debc316c6adb7
FrancescoLuzzi <francescoluzzi99@libero.it>
2024-04-11 22:58:40 +0200

    nvim clean up shit

--- linux_terminal/nvim/init.lua
+++ linux_terminal/nvim/init.lua
@@ -182,10 +182,6 @@ require('lazy').setup({
   { 'folke/which-key.nvim', opts = {} },
 
   {
-    'mg979/vim-visual-multi',
-  },
-
-  {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     dependencies = { 'folke/which-key.nvim' },
@@ -247,19 +243,24 @@ require('lazy').setup({
   },
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable('make') == 1
-    end,
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable('make') == 1
+        end,
+      },
+    },
   },
 
   {
--- linux_terminal/nvim/lua/custom/plugins/dressing.lua
+++ linux_terminal/nvim/lua/custom/plugins/dressing.lua
@@ -1,6 +1,5 @@
 return {
   'stevearc/dressing.nvim',
-  opts = {},
   init = function()
     require('dressing').setup({
       input = {
