271dbee2333748d6faf70212805584d54ab2c3ff
FrancescoLuzzi <francescoluzzi99@libero.it>
2024-05-11 12:39:05 +0200

    formatting

--- linux_terminal/nvim/lua/custom/plugins/colorscheme.lua
+++ linux_terminal/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
+  'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
   opts = {
     transparent = true,
     styles = {
-      sidebars = "transparent",
-      floats = "normal",
+      sidebars = 'transparent',
+      floats = 'normal',
     },
     on_highlights = function(hl, c)
       hl.TelescopeBorder = { fg = c.border_highlight, bg = c.none }
       hl.TelescopeNormal = { fg = c.fg, bg = c.none }
       hl.TelescopePromptBorder = { fg = c.orange, bg = c.none }
       hl.TelescopePromptTitle = { fg = c.orange, bg = c.none }
-    end
+    end,
   },
   init = function()
     vim.cmd [[colorscheme tokyonight]]
-  end
+  end,
 }
