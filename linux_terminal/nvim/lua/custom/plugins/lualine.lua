665081314b90ba05c44ce72d1a3a4701599c59d4
FrancescoLuzzi <francescoluzzi99@libero.it>
2024-08-27 19:31:19 +0200

    lualine: update separators

--- linux_terminal/nvim/lua/custom/plugins/lualine.lua
+++ linux_terminal/nvim/lua/custom/plugins/lualine.lua
@@ -6,8 +6,8 @@ return {
     options = {
       icons_enabled = true,
       theme = 'tokyonight',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      component_separators = '|',
+      section_separators = { left = '', right = '' },
       globalstatus = true,
     },
     sections = {
