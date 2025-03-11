a99047f1ed15de312998e640134dba8ac44d7c46
FrancescoLuzzi <francescoluzzi99@libero.it>
2025-03-12 00:34:38 +0100

    fix(nvim): configure codelldb for rustaceanvim

--- linux_terminal/nvim/lua/custom/plugins/rustaceanvim.lua
+++ linux_terminal/nvim/lua/custom/plugins/rustaceanvim.lua
@@ -5,24 +5,23 @@ return {
   config = function()
     vim.g.rustaceanvim = function()
       -- Update this path
-      local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/'
-      local codelldb_path = extension_path .. 'adapter/codelldb'
-      local liblldb_path = extension_path .. 'lldb/lib/liblldb'
-      local this_os = vim.uv.os_uname().sysname;
-
-      -- The path is different on Windows
-      if this_os:find "Windows" then
-        codelldb_path = extension_path .. "adapter\\codelldb.exe"
-        liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+      local mason_path = vim.fn.glob(vim.fn.stdpath('data') .. '/mason/')
+      local codelldb_path = mason_path .. 'bin/codelldb'
+      local liblldb_path = mason_path .. 'packages/codelldb/extension/lldb/lib/liblldb'
+      local this_os = vim.loop.os_uname().sysname
+      -- The path in windows is different
+      if this_os:find 'Windows' then
+        codelldb_path = mason_path .. 'packages\\codelldb\\extension\\adapter\\codelldb.exe'
+        liblldb_path = mason_path .. 'packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll'
       else
-        -- The liblldb extension is .so for Linux and .dylib for MacOS
-        liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+        -- The liblldb extension is .so for linux and .dylib for macOS
+        liblldb_path = liblldb_path .. (this_os == 'Linux' and '.so' or '.dylib')
       end
 
       local cfg = require('rustaceanvim.config')
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities =
-          vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
 
       return {
         dap = {
