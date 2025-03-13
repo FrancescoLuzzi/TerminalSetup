7be3c1c41dec446ce1efeab472b42bdd92744a83
FrancescoLuzzi <francescoluzzi99@libero.it>
2025-03-13 21:48:10 +0100

    chore(nvim): update deps and fidget-nvim

--- linux_terminal/nvim/init.lua
+++ linux_terminal/nvim/init.lua
@@ -5,8 +5,7 @@
 if vim.loop.os_uname().sysname:match('Windows') then
   local powershell_options = {
     shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell',
-    shellcmdflag =
-    '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+    shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
     shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
     shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
     shellquote = '',
@@ -149,7 +148,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+      { 'j-hui/fidget.nvim', tag = 'v1.6.1', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -179,7 +178,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',   opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
 
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
@@ -196,23 +195,17 @@ require('lazy').setup({
       },
       on_attach = function(bufnr)
         local gs = require('gitsigns')
-        vim.keymap.set(
-          'n',
-          '[h',
-          function() gs.nav_hunk('prev') end,
-          { buffer = bufnr, desc = 'Go to Previous Git Hunk' }
-        )
-        vim.keymap.set(
-          'n',
-          ']h',
-          function() gs.nav_hunk('next') end,
-          { buffer = bufnr, desc = 'Go to Next Git Hunk' }
-        )
+        vim.keymap.set('n', '[h', function()
+          gs.nav_hunk('prev')
+        end, { buffer = bufnr, desc = 'Go to Previous Git Hunk' })
+        vim.keymap.set('n', ']h', function()
+          gs.nav_hunk('next')
+        end, { buffer = bufnr, desc = 'Go to Next Git Hunk' })
         require('which-key').add({
-          { '<leader>h',  group = 'Hunk & Gitsigns' },
-          { '<leader>hp', gs.preview_hunk_inline,       desc = 'Hunk Preview' },
-          { '<leader>hs', gs.stage_hunk,                desc = 'Stage Hunk' },
-          { '<leader>hr', gs.reset_hunk,                desc = 'Reset Hunk' },
+          { '<leader>h', group = 'Hunk & Gitsigns' },
+          { '<leader>hp', gs.preview_hunk_inline, desc = 'Hunk Preview' },
+          { '<leader>hs', gs.stage_hunk, desc = 'Stage Hunk' },
+          { '<leader>hr', gs.reset_hunk, desc = 'Reset Hunk' },
           { '<leader>hb', gs.toggle_current_line_blame, desc = 'Toggle Blame' },
         })
       end,
@@ -537,7 +530,7 @@ which_key.add({
     desc = 'Close all other windows',
     remap = false,
   },
-  { '<leader>s',  group = 'Search',               remap = false },
+  { '<leader>s', group = 'Search', remap = false },
   {
     '<leader>sa',
     ts_builtin.builtin,
@@ -610,21 +603,16 @@ which_key.add({
     desc = 'Fuzzily search word in buffer',
     remap = false,
   },
-  { '<leader>sW', ts_builtin.grep_string,         desc = 'Word under cursor', remap = false },
-  { '<leader>t',  group = 'Toggle',               remap = false },
-  { '<leader>td', diffview_toggle,                desc = 'Toggle Diffview',   remap = false },
-  { '<leader>tw', ':set wrap!<CR>',               desc = 'Toggle word wrap',  remap = false },
-  { '<leader>x',  ':bn<bar>sp<bar>bp<bar>bd<CR>', desc = 'Close Buffer',      remap = false },
+  { '<leader>sW', ts_builtin.grep_string, desc = 'Word under cursor', remap = false },
+  { '<leader>t', group = 'Toggle', remap = false },
+  { '<leader>td', diffview_toggle, desc = 'Toggle Diffview', remap = false },
+  { '<leader>tw', ':set wrap!<CR>', desc = 'Toggle word wrap', remap = false },
+  { '<leader>x', ':bn<bar>sp<bar>bp<bar>bd<CR>', desc = 'Close Buffer', remap = false },
 })
 
 -- Comment line visual mode
 
-vim.keymap.set(
-  'v',
-  '<leader>/',
-  'gc',
-  { remap = true, desc = 'Comment toggle line (visual)' }
-)
+vim.keymap.set('v', '<leader>/', 'gc', { remap = true, desc = 'Comment toggle line (visual)' })
 
 -- Window and buffers commands
 
